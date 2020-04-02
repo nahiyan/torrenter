8352133f7162137e3a90cd987cc82ca61f721ac9
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-04-02 13:20:11 +0600

    Flip coordinate system of scroll view to make it start from the top.

---
+++ Torrenter/TorrentDetailsContainer.swift
@@ -0,0 +1,13 @@
+//
+//  TorrentDetailsContainer.swift
+//  Torrenter
+//
+//  Created by Nahiyan Alamgir on 4/2/20.
+//  Copyright © 2020 Nahiyan Alamgir. All rights reserved.
+//
+
+import Cocoa
+
+class TorrentDetailsContainer: NSView {
+    override var isFlipped: Bool { true }
+}
