724ff7e1f55958966002898960f8ba9847deb2a9
nahiyan <nahiyan.alamgir@northsouth.edu>
2020-06-13 11:11:45 +0600

    Reorganize content once more.

--- main_site/webpack.config.js
+++ main_site/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './js/index.js',
+  entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
